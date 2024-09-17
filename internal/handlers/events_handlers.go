1de393febf49a89c5eb525485c489176ffd66533
Andrew Coyne <drewcoyne02@gmail.com>
2024-09-16 20:13:26 -0400

    added put handler

--- internal/handlers/events_handlers.go
+++ internal/handlers/events_handlers.go
@@ -32,7 +32,7 @@ func handleGetEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var eventDetails *tools.EventDetails = (*database).GetEvents(chi.URLParam(r, "eventID"))
+	var eventDetails *tools.EventDetails = (*database).GetEvent(chi.URLParam(r, "eventID"))
 	if eventDetails == nil {
 		slog.Error("Could not retrieve event")
 		api.InternalErrorHandler(w)
@@ -51,4 +51,53 @@ func handleGetEvent(w http.ResponseWriter, r *http.Request) {
 		api.InternalErrorHandler(w)
 		return
 	}
+}
+
+func handlePutEvent(w http.ResponseWriter, r *http.Request) {
+	err, shouldReturn := newFunction(r, w)
+	if shouldReturn {
+		return
+	}
+
+	var database *tools.DatabaseInterface
+	database, err = tools.NewDatabase()
+	if err != nil {
+		api.InternalErrorHandler(w)
+		return
+	}
+
+	var eventDetails *tools.EventDetails = (*database).PutEvents(chi.URLParam(r, "eventID"))
+	if eventDetails == nil {
+		slog.Error("Could not update event")
+		api.InternalErrorHandler(w)
+		return
+	}
+
+	var response = api.EventResponse{
+		Code: http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		slog.Error(err.Error())
+		api.InternalErrorHandler(w)
+		return
+	}
+}
+
+func decodeParams[T *interface{}](w http.ResponseWriter, r *http.Request) (T, bool) {
+	var params T = &T{}
+	var decoder *schema.Decoder = schema.NewDecoder()
+	var err error
+
+	err = decoder.Decode(params, r.URL.Query())
+
+	if err != nil {
+		slog.Error(err.Error())
+		api.InternalErrorHandler(w)
+		return nil, true
+	}
+
+	return params, false
 }
--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -15,6 +15,40 @@ func Handler(router *chi.Mux) {
 
 		apiRouter.Route("/events", func(eventRouter chi.Router) {
 			eventRouter.Get("/{eventID}", handleGetEvent)
+			eventRouter.Put("/{eventID}", handlePutEvent)
+			eventRouter.Delete("/{eventID}", handlePostEvent)
+			eventRouter.Post("/", handlePostEvent)
+		})
+
+		apiRouter.Route("/venues", func(venueRouter chi.Router) {
+			venueRouter.Get("/{venueID}", handleGetVenue)
+			venueRouter.Put("/{venueID}", handlePutVenue)
+			venueRouter.Delete("/{venueID}", handlePostVenue)
+			venueRouter.Post("/", handlePostVenue)
+		})
+
+		apiRouter.Route("/plans", func(planRouter chi.Router) {
+			planRouter.Get("/{planID}", handleGetPlan)
+			planRouter.Put("/{planID}", handlePutPlan)
+			planRouter.Delete("/{planID}", handlePostPlan)
+			planRouter.Post("/", handlePostPlan)
+		})
+
+		apiRouter.Route("/users", func(userRouter chi.Router) {
+			userRouter.Get("/{userID}", handleGetUser)
+			userRouter.Put("/{userID}", handlePutUser)
+			userRouter.Delete("/{userID}", handlePostUser)
+			userRouter.Post("/", handlePostUser)
+		})
+
+		apiRouter.Route("/images", func(imageRouter chi.Router) {
+			imageRouter.Post("/", handlePostImage)
+		})
+
+		apiRouter.Route("/recommendations", func(recRouter chi.Router) {
+			recRouter.Get("feed-recs/{userID}", handleGetFeedRec)
+			recRouter.Get("plan-recs/{userID}", handleGetPlanRec)
+			recRouter.Get("event-based-recs/{userID}", handleGetEBRec)
 		})
 	})
 }
